1a7e46d39b36b494695fb0fd86782f7b3b2c1850
freshskates <business.robertcacho@gmail.com>
2021-04-20 11:28:52 -0700

    testing zone for AES DES and 3DES completed

--- Encryption.py
+++ Encryption.py
@@ -1,58 +1,107 @@
 """
 DES AES and 3DES Testing Encryption Zone
-Margin of Error: +- 0.01s
+
+Margin of Error: +- 0.05s
 """
 from des import DesKey
+import pyaes
 from functools import wraps
 import time
 
 
 class MyTools:
+    @staticmethod
     def info(name):
-
         def decorator(func):
-
             def wrapper(*args, **kwargs):
                 start = time.perf_counter()
                 result = func(*args, **kwargs)
                 end = time.perf_counter()
                 print(f"{name} =>\tElapsed: {end - start:0.6f} seconds")
                 return result
-
             return wrapper
         return decorator
 
+    @staticmethod
+    def aes_test_zone(keys, text):
+        for i in keys:
+            temp = AES_Crypt(keys[i], text)
+            print("Testing key: \t{}: {}".format(i, keys[i]))
+            print("Plain text: \t{}\n".format(text))
+            print("Encrypted: \t{}\n".format(temp.encrypt()))
+            print("Decrypted: \t{}\n".format(
+                temp.decrypt()), end='------------------\n')
 
-class DES_Crypt(MyTools):
+    @staticmethod
+    def des_test_zone(keys, text):
+        for i in keys:
+            temp = DES_Crypt(keys[i], text)
+            print("Testing key: \t{}: {}".format(i, keys[i]))
+            print("Plain text: \t{}\n".format(text))
+            print("Encrypted: \t{}\n".format(temp.encrypt()))
+            print("Decrypted: \t{}\n".format(
+                temp.decrypt()), end='------------------\n')
+
+
+class DES_Crypt():
 
     def __init__(self, key: str, text: str):
         self.key = DesKey(bytes(key.encode('utf-8')))
         self.text = bytes(text.encode('utf-8'))
 
-    @MyTools.info("encrypt")
+    @MyTools.info("T encrypt")
     def encrypt(self):
         self.encrypted = self.key.encrypt(self.text, padding=True)
         return self.encrypted
 
-    @MyTools.info("decrypt")
+    @MyTools.info("T decrypt")
     def decrypt(self):
         self.decrypted = self.key.decrypt(self.encrypted, padding=True)
         return self.decrypted
 
 
+class AES_Crypt():
+
+    def __init__(self, key: str, text: str):
+        self.key = key.encode('utf-8')
+        self.text = bytes(text.encode('utf-8'))
+
+    @MyTools.info("T encrypt")
+    def encrypt(self):
+        aes = pyaes.AESModeOfOperationCTR(self.key)
+        self.encrypted = aes.encrypt(text)
+        return self.encrypted
+
+    @MyTools.info("T decrypt")
+    def decrypt(self):
+        aes = pyaes.AESModeOfOperationCTR(self.key)
+        self.decrypted = aes.decrypt(self.encrypted).decode('utf-8')
+        return self.decrypted
+
+
 if __name__ == "__main__":
+
     keys = {
         "single": "some key",
         "triple": "a key for TRIPLE",
         "bytes_24": "a 24-byte key for TRIPLE",
-        "realistic_key": "1234567812345678REAL_KEY"
+        "realistic_key": "1234567812345678REAL_KEY",
+    }
+
+    byte16 = {
+        "key0_16byte": "m2G2hb*XVnC#&=:@",
+        "key1_16byte": "2:[cNLKX89N#j9^+",
+        "key2_16byte": "+D8eS'K#8*25(My$",
+        "key3_16byte": "Z;Y2Fe.PNx8h~]eV",
+        "key4_16byte": "pt4YNLb9+M^m^w**",
+        "key5_16byte": "x4eRGWXFv56m=E-x",
+        "key6_16byte": "wvyEc@Ub9-AXSj3%",
+        "key7_16byte": "&vb!FBFp=SWZ4k$+",
+        "key8_16byte": "N7QbKdm2*3?meVKk",
+        "key9_16byte": "YQ3!@2vr6y2!4dV9",
     }
     text = "emin and robert project, testing for speed"
 
-    for i in keys:
-        single = DES_Crypt(keys[i], text)
-        print("Testing key: \t{}: {}".format(i, keys[i]))
-        print("Plain text: \t{}\n".format(text))
-        print("Encrypted: \t{}\n".format(single.encrypt()))
-        print("Decrypted: \t{}\n".format(
-            single.decrypt()), end='------------------\n')
+    MyTools.aes_test_zone(byte16, text)
+    MyTools.des_test_zone(byte16, text)
+    MyTools.des_test_zone(keys, text)
